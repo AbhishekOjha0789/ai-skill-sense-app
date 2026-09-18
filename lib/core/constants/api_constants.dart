19361e1b853e184a93d2f18a5bc09692f53c2001
Abhishek Ojha <abhishekojha0789@gmail.com>
2026-09-18 20:26:30 +0530

    18/09/2026-2

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'https://ai-skill-sense-backend.onrender.com/api';
+  static const String baseUrl = 'https://ai-skill-sense.onrender.com/api';
   static const String login = '/auth/login';
   static const String register = '/auth/register';
   static const String profile = '/users/profile';
